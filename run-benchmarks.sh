0eebf883252260a5a6a30d550f775087426d0fcc
Samuel Hopkins <sam@crowdprocess.com>
2016-04-20 15:43:14 +0100

    incorrect dataset name

--- run-benchmarks.sh
+++ run-benchmarks.sh
@@ -20,7 +20,7 @@ do
   for l in $libs
   do
     python python_benchmarks/benchmark.py -l $l -d $d -b apply_index_and_write
-    python python_benchmarks/benchmark.py -l $l -d $d -b get_random_stats -t large -n $nruns
+    python python_benchmarks/benchmark.py -l $l -d "$l"_"$d" -b get_random_stats -t large -n $nruns
     python python_benchmarks/benchmark.py -l $l -d "$l"_"$d" -b get_random_cols -t large -n $nruns
   done
 done
